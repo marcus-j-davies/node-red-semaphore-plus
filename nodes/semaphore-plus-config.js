7d1b76e45d0a1d8a424dec9599b4cf63f42bdddc
Marcus Davies <55892693+marcus-j-davies@users.noreply.github.com>
2023-09-12 13:10:24 +0100

    Upload

---
+++ nodes/semaphore-plus-config.js
@@ -0,0 +1,80 @@
+const { Semaphore } = require("@shopify/semaphore");
+module.exports = function (RED) {
+  function Config(Config) {
+    RED.nodes.createNode(this, Config);
+    const self = this;
+
+    self._semaphore = new Semaphore(1);
+
+    let _permit;
+    let _timer;
+    let _callbacks = [];
+    let _fsTime = 0;
+    let _queue = 0;
+
+    function UpdateNodes() {
+      for (let i = 0; i < _callbacks.length; i++) {
+        _callbacks[i].cb(_queue, _fsTime > 0 ? _fsTime : "Disabled");
+      }
+    }
+
+    self.registerStatus = function (Details) {
+      _callbacks.push(Details);
+    };
+
+    self.deRegisterStatus = function (ID) {
+      _callbacks = _callbacks.filter((CB) => CB.id !== ID);
+    };
+
+    self.reset = function (Time) {
+      if (_timer) {
+        clearTimeout(_timer);
+        _timer = undefined;
+      }
+      _fsTime = Time;
+      if (Time > 0) {
+        _timer = setTimeout(() => {
+          if (_permit && !_permit.isReleased) {
+            _permit.release();
+          }
+        }, Time);
+      }
+
+      UpdateNodes();
+    };
+
+    self.take = async function (Time) {
+      _queue++;
+      setTimeout(() => {
+        UpdateNodes();
+      }, 10);
+      const Temp = await self._semaphore.acquire();
+      _queue--;
+      setTimeout(() => {
+        UpdateNodes();
+      }, 10);
+      _permit = Temp;
+      _fsTime = Time;
+      if (Time > 0) {
+        _timer = setTimeout(() => {
+          _permit.release();
+        }, Time);
+      }
+      return;
+    };
+
+    self.release = async function () {
+      if (_timer) {
+        clearTimeout(_timer);
+        _timer = undefined;
+      }
+      if (_permit && !_permit.isReleased) {
+        await _permit.release();
+        return;
+      }
+      return;
+    };
+  }
+
+  RED.nodes.registerType("semaphore-plus-config", Config);
+};
---
+++ nodes/semaphore-plus-fs-reset.js
@@ -0,0 +1,31 @@
+module.exports = function (RED) {
+  function Reset(config) {
+    RED.nodes.createNode(this, config);
+    const self = this;
+    self._Controller = RED.nodes.getNode(config.config);
+
+    const Status = (GlobalQueueLength, FS) => {
+      self.status({
+        fill: GlobalQueueLength > 0 ? "yellow" : "green",
+        shape: "dot",
+        text: `GQ : ${GlobalQueueLength}, LFS (ms) : ${FS}`,
+      });
+    };
+
+    self._Controller.registerStatus({ id: self.id, cb: Status });
+
+    self.on("close", (removed, done) => {
+      self._Controller.deRegisterStatus(self.id);
+      done();
+    });
+
+    self.on("input", (msg, send, done) => {
+      const _Timeout = msg.sp_timeout || config.timeout;
+      self._Controller.reset(parseInt(_Timeout));
+      delete msg.sp_timeout;
+      send(msg);
+      done();
+    });
+  }
+  RED.nodes.registerType("semaphore-plus-fs-reset", Reset);
+};
